62215ebeec7fcc6a2568ab364cb714322eb26ab9
MagicLonW <823110660@qq.com>
2017-11-30 10:44:09 +0800

    文件读取修改

--- app/src/main/java/com/magiclon/unitstrokeproject/db/MySqliteOpenHelper.kt
+++ app/src/main/java/com/magiclon/unitstrokeproject/db/MySqliteOpenHelper.kt
@@ -4,14 +4,10 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Environment
-import android.util.Log
-
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import java.io.InputStream
 import java.io.OutputStream
-import java.util.*
 
 /**
  * 作者：MagicLon
@@ -21,7 +17,6 @@ import java.util.*
  */
 
 class MySqliteOpenHelper(private val mContext: Context) : SQLiteOpenHelper(mContext, DATABASE_NAME, null, DATABASE_VERSION, null) {
-    private val tablename = "unitstroke"
 
     init {
         DB_PATH = File.separator + "data" + Environment.getDataDirectory().absolutePath + File.separator + mContext.packageName + File.separator + "databases" + File.separator
@@ -41,20 +36,16 @@ class MySqliteOpenHelper(private val mContext: Context) : SQLiteOpenHelper(mCont
         }
         val dbFile = File(DB_PATH + DATABASE_NAME)
         if (!dbFile.exists()) {
-            val inputstream: InputStream
             val os: OutputStream
             try {
-                inputstream = mContext.resources.assets.open(DATABASE_NAME)
                 os = FileOutputStream(dbFile)
-                val buffer = ByteArray(BUFFER_SIZE)
-                var length= inputstream.read(buffer,0,buffer.size)
-                while (length > 0) {
-                    os.write(buffer, 0, length)
-                    length= inputstream.read(buffer,0,buffer.size)
+                os.use { out ->
+                    mContext.resources.assets.open(DATABASE_NAME).use {
+                        it.copyTo(out)
+                    }
                 }
                 os.flush()
                 os.close()
-                inputstream.close()
             } catch (e: IOException) {
                 e.printStackTrace()
             }
@@ -66,7 +57,6 @@ class MySqliteOpenHelper(private val mContext: Context) : SQLiteOpenHelper(mCont
     companion object {
         private val DATABASE_NAME = "unitstroke.db"
         private val DATABASE_VERSION = 1
-        private val BUFFER_SIZE = 1024
         private var DB_PATH: String = ""
     }
 }
